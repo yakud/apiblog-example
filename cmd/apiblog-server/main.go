ecc251d9274d9725518915eaaafe6cabf240f74b
yakud <yakudgm@gmail.com>
2018-09-13 13:16:39 +0300

    Benchmark update

--- cmd/apiblog-server/main.go
+++ cmd/apiblog-server/main.go
@@ -14,6 +14,8 @@ func main() {
 	workers := flag.Int("workers", 1, "as int")
 	GQLSchemaFile := flag.String("gql-schema", "schema/schema.graphql", "as string path")
 	serverAddr := flag.String("addr", "0.0.0.0:8080", "as string path")
+	pgAddr := flag.String("pg-addr", "postgres:5432", "as string path")
+	redisAddr := flag.String("redis-addr", "redis:6379", "as string path")
 	flag.Parse()
 
 	config := &app.Config{
@@ -25,11 +27,11 @@ func main() {
 			User:     "pgadmin",
 			Password: "pgadmin",
 			Database: "apiblog",
-			Addr:     "postgres:5432",
+			Addr:     *pgAddr,
 		},
 
 		RedisOptions: &redis.Options{
-			Addr:     "redis:6379",
+			Addr:     *redisAddr,
 			Password: "", // no password set
 			DB:       0,  // use default DB
 		},
