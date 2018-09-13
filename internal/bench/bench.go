fb41acc1334899a3791496db775e9ac853922fcb
yakud <yakudgm@gmail.com>
2018-09-13 12:27:32 +0300

    Benchmark update

--- internal/bench/bench.go
+++ internal/bench/bench.go
@@ -3,23 +3,34 @@ package bench
 import (
 	"bytes"
 	"fmt"
-	"io/ioutil"
 	"net/http"
+	"sync/atomic"
 	"time"
 )
 
 var client = &http.Client{}
 
 func Bench() {
+	var count int32
+
+	go func() {
+		ticker := time.NewTicker(time.Second)
+		for _ := range ticker.C {
+			fmt.Println(count, "events/sec")
+			atomic.StoreInt32(&count, 0)
+		}
+	}()
+
 	for {
 		t := time.Now()
 		queryCreate()
 		fmt.Println(time.Now().Sub(t))
+		atomic.AddInt32(&count, 1)
 	}
 }
 
 func queryCreate() {
-	url := "http://127.0.0.1:8080"
+	url := "http://127.0.0.1:8080/graphql"
 
 	var jsonStr = []byte(`{"query": "mutation{create(name: \"hello\", shortDescr: \"desc\"){ id }}"}`)
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
@@ -31,6 +42,6 @@ func queryCreate() {
 	}
 	defer resp.Body.Close()
 
-	body, _ := ioutil.ReadAll(resp.Body)
-	fmt.Println(string(body))
+	//body, _ := ioutil.ReadAll(resp.Body)
+	//fmt.Println(string(body))
 }
