37f4b3f0a265ceacce19ea9cb17b2e609dff3e4f
yakud <yakudgm@gmail.com>
2018-09-13 13:01:34 +0300

    Benchmark update

--- internal/bench/bench.go
+++ internal/bench/bench.go
@@ -1,18 +1,16 @@
 package bench
 
 import (
-	"bytes"
 	"fmt"
-	"net/http"
 	"sync/atomic"
 	"time"
 
 	"github.com/montanaflynn/stats"
+	"github.com/valyala/fasthttp"
 )
 
-var client = &http.Client{}
+var client = &fasthttp.Client{}
 var jsonStr = []byte(`{"query": "mutation{create(name: \"hello\", shortDescr: \"desc\"){ id }}"}`)
-var req, _ = http.NewRequest("POST", "http://127.0.0.1:8080/graphql", bytes.NewBuffer(jsonStr))
 
 func durationsToFloat64(durations []time.Duration) []float64 {
 	data := make([]float64, len(durations))
@@ -24,7 +22,6 @@ func durationsToFloat64(durations []time.Duration) []float64 {
 }
 
 func Bench() {
-	req.Header.Set("Content-Type", "application/json")
 
 	var count int32
 	durations := make([]time.Duration, 0)
@@ -55,19 +52,25 @@ func Bench() {
 
 	for {
 		t := time.Now()
-		queryCreate()
+		err := queryCreate()
+		if err != nil {
+			continue
+		}
 
 		durations = append(durations, time.Now().Sub(t))
 		atomic.AddInt32(&count, 1)
 	}
 }
 
-func queryCreate() {
-	resp, err := client.Do(req)
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
+func queryCreate() error {
+	req := fasthttp.AcquireRequest()
+	req.SetRequestURI("http://127.0.0.1:8080/graphql")
+	req.SetBody(jsonStr)
+
+	resp := fasthttp.AcquireResponse()
+
+	err := client.Do(req, resp)
+	return err
 
 	//body, _ := ioutil.ReadAll(resp.Body)
 	//fmt.Println(string(body))
